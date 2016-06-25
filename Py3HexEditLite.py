fe84a1d3d47e763a7e7e26fcab7f2a9b3725dd1d
Medic5700 <super_alias_the_first@hotmail.com>
2016-06-25 19:32:03 -0400

    Change variable name "file" -> "filePath"

--- Py3HexEditLite.py
+++ Py3HexEditLite.py
@@ -4,14 +4,17 @@ def getch():
     import msvcrt #windows specific operations
     return msvcrt.getch()
 
+
 if __name__ == "__main__":
-    file = None
+    filePath = None
     try:
-        file = sys.argv[1]
+        filePath = sys.argv[1]
     except:
         print("file has not been passed to argument")
-        file = ""
-        while (os.path.exists(file) == False):
-            file = input("Enter a file path:")
+        filePath = ""
+        while (os.path.exists(filePath) == False):
+            filePath = input("Enter a file path:")
+    
+    
     print("End Program, press key to continue")
     getch()
