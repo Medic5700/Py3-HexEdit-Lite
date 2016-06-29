a8f384725ec5f9b7ed1b3f62e0079121026ee2ea
Medic5700 <super_alias_the_first@hotmail.com>
2016-06-29 17:22:46 -0400

    General code improvements and cleanup

--- Py3HexEditLite.py
+++ Py3HexEditLite.py
@@ -45,18 +45,22 @@ class Debug:
 
 class Keyboard:
     """A small class for handling AND parsing single character keyboard input"""
-    env = None
+    env = None #The detected environment, either "W" for windows, or "L" for linux
+    
+    #This detects what input method would would (windows of linux) based on what imports work/fail
     try:
+        #Windows
         import msvcrt #windows specific operations
         env = "W"
     except:
+        #Unix
         import tty #unix specific operation
         import sys
         import termios #unix specific operation
         env = "L"
     
     def __init__(self):
-        self.regularKeys = "" #TODO: is this needed?
+        #self.regularKeys = ""
         self.escape = {"L":0x1B,"W":0xE0}
         #this looks backwards, but is meant to make it easy to edit these character bindings
         self.specialKeys = {"CTRL+A"  :{"L":0x01      ,"W":0x01      },
@@ -83,24 +87,22 @@ class Keyboard:
             oldSetting = self.termios.tcgetattr(terminalFD)
             try:
                 self.tty.setraw(self.sys.stdin.fileno())
-                ch = self.sys.stdin.read(1)
+                character = self.sys.stdin.read(1)
             except:
                 pass
             finally:
                 self.termios.tcsetattr(terminalFD, self.termios.TCSADRAIN, oldSetting)
-            return ch
+            return character
         
     def getch(self):
         """parses keyboard input, returns str representing keypress (IE: 'a', '1', 'Ctrl+Z')"""
         raw = self._getch()
         output = None
-        #print(str(raw))
         if (ord(raw) == self.escape[self.env]):
             if (self.env == "W"):
-                temp = ord(raw)*256 + ord(self._getch())
+                temp = ord(raw) * 256 + ord(self._getch())
             if (self.env == "L"):
-                temp = ord(raw)*256**2 + ord(self._getch())*256 + ord(self._getch())
-            #print(temp)
+                temp = ord(raw) * 256**2 + ord(self._getch()) * 256 + ord(self._getch())
             for i in self.specialKeys.keys():
                 if (self.specialKeys[i][self.env] == temp):
                     return i
@@ -110,65 +112,24 @@ class Keyboard:
                     return i
         return raw
 
-'''
-class WriteBuffer:
-    def __init__(self):
-        self.actions = []
-        self.redoStack = []
-    def undo(self):
-        """removes last action from action queue (appends to redo stack)"""
-        pass
-    def redo(self):
-        """Appends action from redo stack to action queue"""
-        pass
-    #ef write(self):
-    #   #resets redo buffer
-    #    pass
-    def __setitem__(self,key,value):
-        # https://docs.python.org/3/reference/datamodel.html#object.__setitem__
-        pass
-    def __getitam__(self,key):
-        # https://docs.python.org/3/reference/datamodel.html#object.__getitem__
-        pass
-    def __len__(self):
-        # https://docs.python.org/3/reference/datamodel.html#object.__len__
-        pass
-    def status(self):
-        """returns float representing how 'full' the buffer is as percentage"""
-        pass
-
-class ReadBuffer:
-    def __init__(self):
-        self.fd = None
-        self.blocks = {}
-        self.blockSize = 1024*4
-    def __getitam__(self,key):
-        # https://docs.python.org/3/reference/datamodel.html#object.__getitem__
-        pass
-    def __len__(self):
-        # https://docs.python.org/3/reference/datamodel.html#object.__len__
-        pass
-    def status(self):
-        """returns float representing how 'full' the buffer is as percentage"""
-        pass
-'''
 class Buffer:
     import os
     import math
     def __init__(self, filePath):
         self.filePath = filePath
-        self.file = open(filePath,'r+b') #assume filepath is valid
-        self.blocks = {} #contains {Offset:(lastAccessTime, bytearray)}
+        self.file = open(filePath, 'r+b') #does not handle exceptopns here, so calling function can handle raised exceptions
+        self.blocks = {} #contains block of data stored as {Offset:[array of int 0<=x<=255, or None]}
         self.blockSize = 16 #1024*4
         self.actionQueue = [] #contains actions as an ordered list of tuples (offset, data)
-        self.redoStack = []
+        self.redoStack = [] #contains actions as stack of tuples (offset, data)
+        self.maxBlocks = 8 #4 #maximum number of blocks to hold in memory
         
-        self._cacheMiss(0x00)
+        self._cacheMiss(0x00) #load initial block
         debug.debug("buffer Blocks", self.blocks)
         
     def __delitem__(self,key):
         # https://docs.python.org/3/reference/datamodel.html#object.__delitem__
-        self.actionQueue.append((key,None))
+        self.actionQueue.append((key, None))
         
     def __getitem__(self,key):
         """returns array of ints (NOT BYTES)
@@ -177,14 +138,15 @@ class Buffer:
         DO NOT itterate over with a for loop, IndexError will not be raised
         """
         # https://docs.python.org/3/reference/datamodel.html#object.__getitem__
-        if (isinstance(key,slice)):
+        if (isinstance(key, slice)):
             #print("Slice stuff:", key.start, key.stop, key.step)
+            #TODO: implement full functionality (raise errors, handle cases ([1:],[:1],[:],[-1:],etc)
             step = 1
             if (key.step != None):
                 step = key.step
             i = key.start
             temp = []
-            while (i<key.stop):
+            while (i < key.stop):
                 if (self._inCache(i) == False):
                     self._cacheMiss(i)
                 temp.append(self.blocks[(i // self.blockSize) * self.blockSize][i - (i // self.blockSize) * self.blockSize])
@@ -193,8 +155,6 @@ class Buffer:
             for i in self.actionQueue:
                 if ((i[0] >= key.start) and (i[0] < key.stop) and (i[0] % step == key.start % step)):
                     temp[(i[0] - key.start) // step] = i[1]
-                
-                
             return temp
         else: #it's a regular int
             if (self._inCache(key) == False):
@@ -213,6 +173,7 @@ class Buffer:
             if (i[0] > temp):
                 temp = i[0]
         return temp
+    
     def __setitem__(self,key,value):
         # https://docs.python.org/3/reference/datamodel.html#object.__setitem__
         self.actionQueue.append((key,value))
@@ -227,20 +188,21 @@ class Buffer:
         """loads a block of the file into memeory"""
         #Assume _inCache() has already been called
         closestBlock = (offset // self.blockSize) * self.blockSize
-        if (len(self.blocks) >= 8):
+        if (len(self.blocks) >= self.maxBlocks):
             self._cacheEvict(closestBlock)
         fileSize = self.os.path.getsize(self.filePath)
         block = []
         if (closestBlock > fileSize):
-            block = [None for i in range(0,self.blockSize)]
+            block = [None for i in range(0, self.blockSize)]
         else:
             self.file.seek(closestBlock)
             temp = self.file.read(self.blockSize)
-            for i in range(0,len(temp)):
-                block.append(temp[i]) #temp[i] is int
-            for i in range(len(temp),self.blockSize):
+            for i in range(0, len(temp)):
+                block.append(temp[i])
+            for i in range(len(temp), self.blockSize):
                 block.append(None)
         self.blocks[closestBlock] = block
+        
     def _cacheEvict(self,current):
         """Takes the current location of read, evicts block from memory that isn't current"""
         # evict furthest block from current block
@@ -263,9 +225,11 @@ class Buffer:
     def flag(self, start, finish):
         """Returns array of bool signifying which bytes have been changed"""
         pass
+    
     def close(self):
         #remember to delete all the buffers
         self.file.close()
+        
     def save(self):
         #if editied file is smaller then original, create a copy to resize
         pass
@@ -274,52 +238,52 @@ def _interface(data, curserLocation, screenLocation):
     """Prints file name, size of file, buffer status, and the hex editing interface"""
     #test interface
     text = "Py3HexEditLite " + version.ljust(8, " ") + "File:" + filePath[-52:].ljust(52," ") + "\n"
-    text += "Size: " + "{0:7.2f}".format(fileSize / int(1024 ** math.floor(math.log(fileSize,1024)))) + " " 
+    text += "Size: " + "{0:7.2f}".format(fileSize / int(1024 ** math.floor(math.log(fileSize, 1024)))) + " " 
     try: #Just in case 2**10 years in the future, someone decides to open the entirety of the internet worth of a file in this hex editor...
-        text += " KMGTPEZY"[int(math.floor(math.log(fileSize,1024)))] + "B"
+        text += " KMGTPEZY"[int(math.floor(math.log(fileSize, 1024)))] + "B"
     except IndexError:
         text += "?B"
     text += " Buffer:XXX%|XXX% Location:" + ("0x" + hex(math.floor(curserLocation))[2:].upper()).rjust(18, " ")
     text += "/" + ("0x" + hex(fileSize)[2:].upper()).rjust(18, " ") + "\n"
-    for i in range(screenLocation//16,screenLocation//16 + 16): #TODO: this is a shortcut, fix it
-        temp = hex(i*16)[2:].zfill(12) + "|"
-        for j in range(0,8):
+    for i in range(screenLocation // 16, screenLocation // 16 + 16): #TODO: this is a shortcut, fix it
+        temp = hex(i * 16)[2:].upper().zfill(12) + "|"
+        for j in range(0, 8):
             temp += " "
-            if (curserLocation == i*16+j): #large 4 bits
+            if (curserLocation == i * 16 + j): #large 4 bits
                 temp += "-"
-            elif (data[i*16+j] == None):
+            elif (data[i * 16 + j] == None):
                 temp += "_"
             else:
-                temp += hex(data[i*16+j] // 16)[2:].upper()
+                temp += hex(data[i * 16 + j] // 16)[2:].upper()
             
-            if (curserLocation == i*16+j+0.5): #small 4 bits
+            if (curserLocation == i * 16 + j + 0.5): #small 4 bits
                 temp += "-"
-            elif (data[i*16+j] == None):
+            elif (data[i * 16 + j] == None):
                 temp += "_"
             else:
-                temp += hex(data[i*16+j] % 16)[2:].upper()
+                temp += hex(data[i * 16 + j] % 16)[2:].upper()
         temp += "|"
-        for j in range(8,16):
+        for j in range(8, 16):
             temp += " "
-            if (curserLocation == i*16+j): #large 4 bits
+            if (curserLocation == i * 16 + j): #large 4 bits
                 temp += "-"
-            elif (data[i*16+j] == None):
+            elif (data[i * 16 + j] == None):
                 temp += "_"
             else:
-                temp += hex(data[i*16+j] // 16)[2:].upper()
+                temp += hex(data[i * 16 + j] // 16)[2:].upper()
             
-            if (curserLocation == i*16+j+0.5): #small 4 bits
+            if (curserLocation == i * 16 + j + 0.5): #small 4 bits
                 temp += "-"
-            elif (data[i*16+j] == None):
+            elif (data[i * 16 + j] == None):
                 temp += "_"
             else:
-                temp += hex(data[i*16+j] % 16)[2:].upper()
+                temp += hex(data[i * 16 + j] % 16)[2:].upper()
         temp += "| "
-        for j in range(0,16):
-            if (data[i*16+j] == None):
+        for j in range(0, 16):
+            if (data[i * 16 + j] == None):
                 temp += " "
-            elif chr(data[i*16+j]).isprintable():
-                temp += chr(data[i*16+j])
+            elif chr(data[i * 16 + j]).isprintable():
+                temp += chr(data[i * 16 + j])
             else:
                 temp += "."
         text += temp + "\n"
@@ -327,49 +291,49 @@ def _interface(data, curserLocation, screenLocation):
     print(text)
 
 def _up():
-    """Move curser up: takes current curser/screen location, returns new curser/screen location"""
+    """Move curser up, sets curserLocation, adjusts screenLocation as needed"""
     global curserLocation
     global screenLocation    
     if (screenLocation > curserLocation - 16):
-        temp = max(0, screenLocation - 16)
+        y = max(0, screenLocation - 16)
     else:
-        temp = screenLocation
+        y = screenLocation
     if (curserLocation < 16):
-        temp2 = curserLocation
+        x = curserLocation
     else:
-        temp2 = curserLocation - 16
-    curserLocation = temp2
-    screenLocation = temp
+        x = curserLocation - 16
+    curserLocation = x
+    screenLocation = y
 def _down():
-    """Move curser down: takes current curser/screen location, returns new curser/screen location"""
+    """Move curser down, sets curserLocation, adjusts screenLocation as needed"""
     global curserLocation
     global screenLocation    
     if (((curserLocation + 16)// 16) * 16 - screenLocation) >= 256:
-        temp = screenLocation + 16
+        y = screenLocation + 16
     else:
-        temp = screenLocation
+        y = screenLocation
     curserLocation = curserLocation + 16
-    screenLocation = temp
+    screenLocation = y
 def _left():
-    """Move curser left: takes current curser/screen location, returns new curser/screen location"""
+    """Move curser left, sets curserLocation, adjusts screenLocation as needed"""
     global curserLocation
     global screenLocation    
     if (screenLocation > curserLocation - 0.5):
-        temp = max(0, screenLocation - 16)
+        y = max(0, screenLocation - 16)
     else:
-        temp = screenLocation
+        y = screenLocation
     curserLocation = max(0, curserLocation - 0.5)
-    screenLocation = temp
+    screenLocation = y
 def _right():
-    """Move curser right: takes current curser/screen location, returns new curser/screen location"""
+    """Move curser right, sets curserLocation, adjusts screenLocation as needed"""
     global curserLocation
     global screenLocation    
     if (((curserLocation + 0.5)// 16) * 16 - screenLocation) >= 256:
-        temp = screenLocation + 16
+        y = screenLocation + 16
     else:
-        temp = screenLocation
+        y = screenLocation
     curserLocation = curserLocation + 0.5
-    screenLocation = temp
+    screenLocation = y
 
 # API, accessable by user in 'Input' mode
 def save():
@@ -398,26 +362,46 @@ def find(x):
 def _write(location, byte):
     global curserLocation
     global screenLocation
-    debug.debug("_write", location, byte)
+    #debug.debug("_write", location, byte)
     
     if (byte == None):
         buffer[int(math.floor(location))] = None
     elif ((location - math.floor(location)) == 0):
         number = int(byte,16)
-        buffer[int(math.floor(location))] = number*16 + (buffer[int(math.floor(location))] % 16)
+        buffer[int(math.floor(location))] = number * 16 + (buffer[int(math.floor(location))] % 16)
         _right()
     elif ((location - math.floor(location)) == 0.5):
         number = int(byte,16)
         buffer[int(math.floor(location))] = (buffer[int(math.floor(location))] // 16) * 16 + number
         _right()
-    else:
-        return "Write Error"
-    return 1
-        
+
 if __name__ == "__main__":
     debug = Debug(True)
     print("Starting Py3HexEditLite.py")
     debug.debug("Starting Py3HexEditLite.py===================================")
+    '''
+    #Buffer tests
+    buffer = Buffer("ReadMe.txt")
+    debug.debug("buffer length:", len(buffer))
+    debug.debug("buffer[0:188]:", buffer[0:188])
+    debug.debug("buffer[180:200]:", buffer[180:200])
+    debug.debug("buffer[180]:", buffer[180])
+    debug.debug("buffer[0]:", buffer[0])
+    debug.debug("buffer[200]:", buffer[200])
+    buffer[200] = 10
+    debug.debug("write buffer[200]:", buffer[200])
+    debug.debug("buffer[199:202]:", buffer[199:202])
+    debug.debug("len(buffer)", len(buffer))
+    buffer[0] = 255
+    debug.debug("buffer[0]:", buffer[0])
+    debug.debug("buffer[0:20]:", buffer[0:20])
+    for i in range(0,20):
+        buffer[i] = i
+    debug.debug("buffer[0:20]:", buffer[0:20])
+    del(buffer[0])
+    debug.debug("buffer[0:20]:", buffer[0:20])
+    buffer.close()
+    '''
     print("Py3HexEditLite " + version + " has started")
     
     curserLocation = 0.0 #A real, since in hex, a byte is represented as 2 hex chars
@@ -451,7 +435,7 @@ if __name__ == "__main__":
         _interface(buffer, curserLocation, screenLocation)
         
         raw = keyboard.getch()
-        debug.debug("variable \"raw\"", type(raw),len(raw),str(raw))
+        #debug.debug("variable \"raw\"",  type(raw), len(raw), str(raw))
         if (raw == "UP"):
             _up()
         elif (raw == "DOWN"):
@@ -464,7 +448,7 @@ if __name__ == "__main__":
             if (mode == "Hex"):
                 mode = "Text"
             elif (mode == "Text"):
-                mode = "Hex"         
+                mode = "Hex"
         #elif (raw == "CTRL+E"): #TODO
         #    mode = "Input"
         elif (raw == "DEL"):
